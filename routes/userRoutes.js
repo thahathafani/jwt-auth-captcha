e4115baacee043a914eb1865601c921f76907515
thahathafani <thahafazzin@gmail.com>
2024-07-28 13:06:22 +0530

    added routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { registerUser, loginUser } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 
@@ -10,5 +12,11 @@ router.post('/register', registerUser);
 // Login Routes
 
 router.post('/login', loginUser);
+
+// Protected Route Example
+router.get('/profile', protect, (req, res) => {
+    res.json(req.user);
+
+});
 
 module.exports = router;
